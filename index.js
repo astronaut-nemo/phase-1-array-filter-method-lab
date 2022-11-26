7d84b718afa697d4cc05fbc6690c113572383240
Joanna <astronautnemo@gmail.com>
2022-11-26 20:25:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(array, string){
+    return array.filter(function (name){
+      return (name.toUpperCase() === string.toUpperCase())
+    })
+}
+
+
+function fuzzyMatch(array, string){
+    return array.filter(function (name){
+        return (name.slice(0, 2) === string)
+      })
+}
+
+function matchName(array, string){
+    return array.filter(function (element){
+      return(element.name === string)
+    })
+}
